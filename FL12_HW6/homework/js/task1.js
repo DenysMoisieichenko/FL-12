1a95ab16a53485a33fb213a14155846a916fb2d1
Denys Moisieichenko <mdenalex@gmail.com>
2019-12-20 18:32:52 +0200

    adding changes after QA session

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -4,24 +4,20 @@ let c = prompt('Enter the "c" coefficient for the quadratic equation "a*x**2 + b
 
 if (a === '' || b === '' || c === '' || a === 0 || isNaN(a) || isNaN(b) || isNaN(c)) {
     alert('Invalid input data');
-    console.log('Invalid input data');
 } else {
     let discriminant = b * b - 4 * a * c;
     let denominator = 2 * a;
     
     if (discriminant === 0) {
-        let x = -b / denominator;
+        let x = Math.round(- b / denominator);
 
         alert(`x = ${x}`);
-        console.log(`x = ${x}`);
     } else if (discriminant > 0) {
-        let x1 = ( - b + Math.sqrt(discriminant) ) / denominator;
-        let x2 = ( - b - Math.sqrt(discriminant) ) / denominator;
+        let x1 = Math.round( ( - b + Math.sqrt(discriminant) ) / denominator );
+        let x2 = Math.round( ( - b - Math.sqrt(discriminant) ) / denominator );
 
         alert(`x1 = ${x1} and x2 = ${x2} `);
-        console.log(`x1 = ${x1} and x2 = ${x2}`);
     } else {
         alert('no solution');
-        console.log('no solution');
     }
 }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,17 +1,17 @@
-let a = prompt('enter length of the first side of the triangle');
-let b = prompt('enter length of the second side of the triangle');
-let c = prompt('enter length of the third side of the triangle');
+let a = parseInt( prompt('enter length of the first side of the triangle') );
+let b = parseInt( prompt('enter length of the second side of the triangle') );
+let c = parseInt( prompt('enter length of the third side of the triangle') );
 
 if (a === '' || b === '' || c === '' || isNaN(a) || isNaN(b) || isNaN(c)) {
     alert('input values should be ONLY numbers');
 } else if (a === 0 || b === 0 || c === 0) {
     alert('A triangle must have 3 sides with a positive definite length');
 } else if ( !(a + b > c) && !(a + c > b) && !(b + c > a) ) {
-    console.log('Triangle doesn’t exist');
+    alert('Triangle doesn’t exist');
 } else if (a === b && a === c && b === c) {
-    console.log('Equilateral triangle');
+    alert('Equilateral triangle');
 } else if (a === b || a === c || b === c) {
-    console.log('Isosceles triangle');
+    alert('Isosceles triangle');
 } else {
-    console.log('Scalene triangle');
+    alert('Scalene triangle');
 }
