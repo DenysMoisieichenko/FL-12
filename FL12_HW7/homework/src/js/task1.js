fe167fea54dca5c5ee8124eda8abe1069872f378
Denys Moisieichenko <mdenalex@gmail.com>
2019-12-25 15:36:12 +0200

    HW7 v 1.0

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,1 +1,63 @@
-// Your code goes here
+let email;
+let emailMinLength = 5;
+let passwordMinLength = 6;
+
+do {
+    email = prompt('Please enter Your email', 'example@gmail.com');
+    
+    if (email === '' || email === null) {
+        alert('Canceled.');
+        break;
+    } else if (email.length < emailMinLength) {
+        alert("I don't know any emails having name length less than 5 symbols");
+    } else if (email === 'user@gmail.com' || email === 'admin@gmail.com' ) {
+        let password = prompt('Please enter Your password','password');
+
+        if (password === '' || password === null) {
+            alert('Canceled.');
+            break;
+        } else if (email === 'user@gmail.com' && password === 'UserPass'
+               || email === 'admin@gmail.com' && password === 'AdminPass') {
+            let changePassword = confirm('Do You want to change Your password?');
+
+            if (changePassword === false) {
+                alert('You have failed the change.');
+                break;
+            } else {
+                let oldPassword = prompt('Write Your old password', 'old password');
+
+                if (oldPassword === '' || oldPassword === null) {
+                    alert('Canceled.');
+                    break;
+                } else if (email === 'user@gmail.com' && oldPassword === 'UserPass'
+                       || email === 'admin@gmail.com' && oldPassword === 'AdminPass') {
+                    let newPassword = prompt('Enter Your new password', 'new password');
+
+                    if (newPassword === '' || newPassword === null) {
+                        alert('Canceled.');
+                        break;
+                    } else if (newPassword.length < passwordMinLength) {
+                        alert("It's too short password. Sorry.");
+                    } else {
+                        let confirmNewPassword = prompt('Write Your new password again');
+
+                        if (confirmNewPassword !== newPassword) {
+                            alert('You wrote the wrong password.');
+                            break;
+                        } else {
+                            alert('You have successfully changed your password.');
+                            break;
+                        }
+                    }
+                } else {
+                    alert('Wrong password');
+                }
+            }
+        } else {
+            alert('Wrong password');
+        }
+    } else {
+        alert("I don't know you");
+    }
+    email++;
+} while (!email);
--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,71 @@
-// Your code goes here
+let playGame = confirm('Do you want to play a game?');
+
+second: 
+if (playGame === false) {
+    alert('You did not become a billionaire, but can.');
+} else {
+    let minNumber = 0;
+    let maxNumber = 8;
+    let totalPrize = 0;
+    //this names used for fixing eslint's 'magic number' warnings
+    let prizeFirst = 100;
+    let prizeSecond = 50;
+    let prizeThird = 25;
+    let prize = [0, prizeThird, prizeSecond, prizeFirst];
+    let chooseRandomNumber;
+    const two = 2;
+    const four = 4;
+
+    do {
+        let randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+        let attempts = 3;
+        
+        first:
+        for (let i = 3; i >= 0; i--) {
+            if (i === 0) {
+                alert(`Thank you for your participation. Your prize is: ${totalPrize}$`);
+                let playAgain = confirm('Do You want to play again?');
+
+                if (playAgain === true) {
+                    break first;
+                } else {
+                    break second;
+                }
+            }
+            
+            chooseRandomNumber = parseInt( prompt(
+            `Choose a roulette pocket number from 0 to ${maxNumber}
+            \nAttempts left: ${attempts}
+            \nTotal prize: ${totalPrize}$
+            \nPossible prize on current attempt: ${prize[i]}$`, '0') );
+
+            if (randomNumber === chooseRandomNumber) {
+                totalPrize += prize[i];
+                let playMore = confirm(`Congratulation, you won!\nYour prize is: ${prize[i]}$.
+                                \nDo you want to continue?`);
+                
+                if (playMore === false) {
+                    alert(`Thank you for your participation. Your prize is: ${totalPrize}$`);
+                    let playAgain = confirm('Do You want to play again?');
+
+                    if (playAgain === true) {
+                        break first;
+                    } else {
+                        break second;
+                    }
+                } else {
+                    maxNumber += four;
+
+                    for (let i = 1; i < four; i++) {
+                        prize[i] *= two;
+                    }
+                    
+                    randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+                    break first;
+                }
+            }
+            attempts--;
+        }
+        chooseRandomNumber++;
+    } while (chooseRandomNumber);
+}
